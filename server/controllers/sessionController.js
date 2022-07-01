4ba79d4c15e1ae26b5fa753dcfc927b5abfaba76
EdnaK18 <reginakwanrx@gmail.com>
2022-07-01 19:33:48 -0400

    added file for user schema, exported usercontroller and router

---
+++ server/controllers/sessionController.js
@@ -0,0 +1,9 @@
+const sessionController = {}
+
+//middleware to create a session after login
+
+//middleware to check for an active session
+
+//middleware to end a session on logout
+
+module.exports = sessionController;
---
+++ server/controllers/userController.js
@@ -0,0 +1,14 @@
+const userController = {};
+
+//middleware to create a user
+
+//middleware to login
+
+//middleware to find all users
+
+//middleware to update password
+
+//middleware to logout
+
+
+module.exports = userController;
